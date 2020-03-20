5b1f7f089ea6a72e4bd22a6f468028be71f03e30
Stephen Toms <stoms@DIGI5.local>
2020-03-20 16:39:47 -0400

    update case study breadcrumb elements

--- pages/works/studio-eleven.vue
+++ pages/works/studio-eleven.vue
@@ -18,9 +18,10 @@
             <h2 id="ribbon-title" class="pt-1 my-3 mx-md-5" style="font-size: 1em; font-weight: 500;">Studio Eleven
               <span style="color: #585858; font-weight: 300; font-size: 1em; margin-left: .5em;">Towson, Maryland</span>
             </h2>
-            <h2 class="my-auto mx-sm-5" style="font-size: 1em; font-weight: 300; letter-spacing: .05em;"><a
-                class="link d-none d-sm-block" href="https://studioeleven.us">visit site <i
-                  class="fas fa-arrow-right"></i></a></h2>
+            <h2 class="my-auto mx-sm-5" style="font-size: 1em; font-weight: 300; letter-spacing: .05em;">
+              <!-- <a class="link d-none d-sm-block" href="https://studioeleven.us">visit site <i
+                  class="fas fa-arrow-right"></i></a> -->
+            </h2>
           </div>
           <div class="page-container">
             <div class="row my-5 py-5 justify-content-around">
@@ -31,7 +32,7 @@
                     QUICK WORDS
                   </span>
                   <br>
-                  <span class="gordita" style="color:white;">
+                  <span class="" style="color:white;">
                     Behind the scenes creative genius responsible for a Salon’s rebranding. Worked with models, and the
                     salon owners to establish their brand identity through capturing images, and social media.
                     <br>
@@ -84,16 +85,16 @@
                     ESTABLISHING A BRAND
                   </span>
                   <br>
-                  <span class="gordita">
+                  <span class="">
                     Working with the Studio Eleven logo, a bright color pallet was established. Pastels were chosen to
                     keep a light and feminine design.
                   </span>
                 </p>
               </div>
-              <div class="px-5 my-auto col-12 col-lg-6" style="background-position: 100% 27%; ">
+              <div class="px-5 my-auto col-12 col-lg-6 d-flex mx-auto" style="background-position: 100% 27%; ">
                 <img style="max-width: 100%;" :src="require('@/assets/case-studies/studio-eleven/logo.png')">
               </div>
-              <div class="col-12 mt-5">
+              <div class="col-12 mt-5 text-center">
                 <h3>COLOR</h3>
               </div>
               <div class="col-12 mt-4">
@@ -126,7 +127,7 @@
                     TELLING A STORY
                   </span>
                   <br>
-                  <span class="gordita" style="color:white;">
+                  <span class="" style="color:white;">
                     Part of the branding experience was establishing a modern look for the company. With this goal in
                     mind, while capturing photography and videography, this was created by posing the models
                     organically.
@@ -161,7 +162,7 @@
                     FINAL RESULTS
                   </span>
                   <br>
-                  <span class="gordita" style="color:white;">
+                  <span class="" style="color:white;">
                     After establishing brand, creating fresh content, a simple site was designed. The provided logo was
                     broken apart to convey the brand through the website as a whole instead of staying locked in the
                     logo area. Every color used was of a light pastel to keep a soft feel. While laying in content and
@@ -219,14 +220,48 @@
                   :src="require('@/assets/case-studies/studio-eleven/mobile/4.jpg')">
               </div>
             </div>
-            <router-link class="no-dec" to="/callie-tomblin">
+            <div class="row justify-content-center mt-5 mb-lg-3 pt-5">
+              <h3 class="text-white">See more of our work!</h3>
+            </div>
+            <div class="row justify-content-around py-5 mt-lg-5">
+              <router-link to="/callie-tomblin"
+               :style="{ backgroundImage: 'url(' + require('@/assets/emc-mobile.jpg') + ')' }"
+               style="min-height: 400px; background-color: mintcream; min-width: 400px; background-size: cover; background-position: center; " 
+               class="col-4 no-dec px-5 my-5 my-lg-0"
+              >
+                <div style="position: absolute; left: 0; right: 0; background-color: #BBC5A1; width: 100%; height: 100%; opacity: .85;"></div>
+                <h2 class="text-white pt-5 mt-4 font-weight-bolder" style="position: relative;">EMC Performing Arts Stuido</h2>
+                <p class="font-weight-light text-white" style="z-index: 100; position: relative; font-size: 1.2em !important;">
+                  Creating digital content for a website and social media
+                </p>
+                <button class="mx-auto text-center d-block mb-5" style="position: relative; background: none; color: white; border: solid 2px #fbbc00; padding: 0.5em 1em;">View Work</button>
+              </router-link>
+
+              <router-link to="/callie-tomblin"
+               :style="{ backgroundImage: 'url(' + require('@/assets/emc-mobile.jpg') + ')' }"
+               style="min-height: 400px; background-color: mintcream; min-width: 400px; background-size: cover; background-position: center; " 
+               class="col-4 no-dec px-5 my-5 my-lg-0"
+              >
+                <div style="position: absolute; left: 0; right: 0; background-color: #BBC5A1; width: 100%; height: 100%; opacity: .85;"></div>
+                <h2 class="text-white pt-5 mt-4 font-weight-bolder" style="position: relative;">EMC Performing Arts Stuido</h2>
+                <p class="font-weight-light text-white" style="z-index: 100; position: relative; font-size: 1.2em !important;">
+                  Creating digital content for a website and social media
+                </p>
+                <button class="mx-auto text-center d-block mb-5" style="position: relative; background: none; color: white; border: solid 2px #fbbc00; padding: 0.5em 1em;">View Work</button>
+              </router-link>
+              
+            </div>
+             <div class="row mb-5 pt-4 pb-5">
+              <a href="#" class="cta-button mx-auto" style="text-decoration: underline;">View all case studies</a>
+            </div>
+            <!-- <router-link class="no-dec" to="/callie-tomblin">
               <div class="row mt-5 pt-5 breakout justify-content-center">
                 <div class="col mt-5" style="background-color: #252525;">
                   <h4 class="no-dec my-auto " style="color: rgb(165, 165, 165); padding-top: 15%; padding-bottom: 15%;">
                     Next Project: <br><span style="color: white;">Callie Tomblin</span></h4>
                 </div>
               </div>
-            </router-link>
+            </router-link> -->
           </div>
         </div>
 
